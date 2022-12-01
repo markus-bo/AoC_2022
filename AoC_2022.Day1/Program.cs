a41a1b004f1b83fd15eef1789ece86f2a66ac4d4
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2022-12-01 08:32:31 +0100

    Day 1 part 1 puzzle done

--- AoC_2022.Day1/Program.cs
+++ AoC_2022.Day1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Advent Of Code Day 1");
+
+var inputPath = $"{Environment.CurrentDirectory}\\Input1.txt";
+var elveInventory = new Dictionary<int, List<int>>();
+
+var index = 0;
+
+using (var file = new StreamReader(inputPath))
+{
+    while (!file.EndOfStream)
+    {
+        var line = file.ReadLine();
+
+        if (line == "")
+        {
+            index++;
+            continue;
+        }
+
+        if (elveInventory.ContainsKey(index))
+        {
+            elveInventory[index].Add(int.Parse(line!));
+        }
+        else
+        {
+            elveInventory.Add(index, new List<int> { int.Parse(line!) });
+        }
+    }
+}
+
+var maxInventoryElve = elveInventory
+    .Select(x => (index: x.Key, allocInventory: x.Value.Sum()))
+    .OrderByDescending(x => x.allocInventory)
+    .First();
+
+Console.WriteLine($"{maxInventoryElve.index} {maxInventoryElve.allocInventory}");
+
+Console.ReadKey();
