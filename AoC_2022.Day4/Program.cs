ef0960f3cdd1cce97e8e07f9701ff41e6f7c511f
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2022-12-04 13:51:48 +0100

    Solution Day 4

---
+++ AoC_2022.Day4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        var inputPath = $"{Environment.CurrentDirectory}\\Input1.txt";
+
+        var input = new List<string>();
+
+        using (var file = new StreamReader(inputPath))
+        {
+            while (!file.EndOfStream)
+            {
+                var line = file.ReadLine();
+
+                input.Add(line);
+            }
+        }
+
+        Console.WriteLine(solutionPart1(input));
+        Console.WriteLine(solutionPart2(input));
+    }
+
+    static int solutionPart1(List<string> input)
+    {
+        var completeOverlaps = 0;
+
+        foreach (var assignement in input)
+        {
+            var left = assignement.Split(',')[0];
+            var right = assignement.Split(',')[1];
+
+            var minLeft = int.Parse(left.Split('-')[0]);
+            var maxLeft = int.Parse(left.Split('-')[1]);
+            var minRight = int.Parse(right.Split('-')[0]);
+            var maxRight = int.Parse(right.Split('-')[1]);
+
+            if ( (minLeft >= minRight && maxLeft <= maxRight) ||
+                 (minRight >= minLeft && maxRight <= maxLeft))
+                completeOverlaps++;
+        }
+
+        return completeOverlaps;
+    }
+
+    static int solutionPart2(List<string> input)
+    {
+        var partialOverlaps = 0;
+
+        foreach (var assignement in input)
+        {
+            var left = assignement.Split(',')[0];
+            var right = assignement.Split(',')[1];
+
+            var minLeft = int.Parse(left.Split('-')[0]);
+            var maxLeft = int.Parse(left.Split('-')[1]);
+            var minRight = int.Parse(right.Split('-')[0]);
+            var maxRight = int.Parse(right.Split('-')[1]);
+
+            if ( (minLeft >= minRight && minLeft <= maxRight) ||
+                 (maxLeft >= minRight && maxLeft <= maxRight) ||
+                 (minRight >= minLeft && minRight <= maxLeft) ||
+                 (maxRight >= minLeft && maxRight <= maxLeft) )
+                partialOverlaps++;
+        }
+
+        return partialOverlaps;
+    }
+}
