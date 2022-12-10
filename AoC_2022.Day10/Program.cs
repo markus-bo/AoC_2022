0b1da4b04235a0be403e4dac06a94a29e5612252
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2022-12-10 15:06:57 +0100

    Solution Day 10

--- AoC_2022.Day10/Program.cs
+++ AoC_2022.Day10/Program.cs
@@ -7,7 +7,7 @@ class Solution
         var input0 = GetInput($"{Environment.CurrentDirectory}\\Input0.txt");
         var input1 = GetInput($"{Environment.CurrentDirectory}\\Input1.txt");
 
-        Console.WriteLine($"Part 1, input 0a: '{solutionPart1(input0)}'");
+        Console.WriteLine($"Part 1, input 0: '{solutionPart1(input0)}'");
         Console.WriteLine($"Part 1, input 1: '{solutionPart1(input1)}'");
         Console.WriteLine($"Part 2, input 0: '{solutionPart2(input0)}'");
         Console.WriteLine($"Part 2, input 1: '{solutionPart2(input1)}'");
