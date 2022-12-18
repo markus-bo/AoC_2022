10a3a8f88ed521d212c170da449244983dad389f
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2022-12-18 12:44:15 +0100

    Solution Day 18 Part 1

--- AoC_2022.Day18/Program.cs
+++ AoC_2022.Day18/Program.cs
@@ -1,4 +1,29 @@
-﻿class Solution
+﻿class Point
+{
+    public long X { get; set; }
+    public long Y { get; set; }
+
+    public long Z { get; set; }
+
+    public Point(long x, long y, long z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public Point(IEnumerable<long> vector)
+    {
+        if (vector.Count() != 3)
+            throw new ArgumentOutOfRangeException();
+
+        this.X = vector.First();
+        this.Y = vector.Skip(1).First();
+        this.Z = vector.Skip(2).First();
+    }
+}
+
+class Solution
 {
     static void Main(string[] args)
     {
@@ -13,7 +38,34 @@
 
     static string solutionPart1(string[] input)
     {
-        return "";
+        var points = input.Select(l => l.Split(',').Select(long.Parse))
+             .Select(v => new Point(v))
+             .ToList();
+
+        var minX = points.Select(p => p.X).Min();
+        var maxX = points.Select(p => p.X).Max();
+        var minY = points.Select(p => p.Y).Min();
+        var maxY = points.Select(p => p.Y).Max();
+        var minZ = points.Select(p => p.Z).Min();
+        var maxZ = points.Select(p => p.Z).Max();
+
+        var faceCount = points.Count * 6;
+
+        for (int i = 0; i < points.Count() - 1; i++)
+        {
+            for (int j = i + 1; j < points.Count(); j++)
+            {
+                if ((points[i].X + 1 == points[j].X || points[i].X - 1 == points[j].X) && points[i].Y == points[j].Y && points[i].Z == points[j].Z)
+                    faceCount -= 2;
+
+                if ((points[i].Y + 1 == points[j].Y || points[i].Y - 1 == points[j].Y) && points[i].X == points[j].X && points[i].Z == points[j].Z)
+                    faceCount -= 2;
+
+                if ((points[i].Z + 1 == points[j].Z || points[i].Z - 1 == points[j].Z) && points[i].X == points[j].X && points[i].Y == points[j].Y)
+                    faceCount -= 2;
+            }
+        }
+        return faceCount.ToString();
     }
 
     static string solutionPart2(string[] input)
