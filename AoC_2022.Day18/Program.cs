2efdda611dc4142bfef8cecb8152f0701caa94bb
Markus Bodlos <74670566+markus-bo@users.noreply.github.com>
2022-12-19 09:07:09 +0100

    Day 18 Part 2 started

--- AoC_2022.Day18/Program.cs
+++ AoC_2022.Day18/Program.cs
@@ -1,4 +1,6 @@
-﻿class Point
+﻿using System.Diagnostics.CodeAnalysis;
+
+record Point
 {
     public long X { get; set; }
     public long Y { get; set; }
@@ -70,7 +72,132 @@ class Solution
 
     static string solutionPart2(string[] input)
     {
-        return "";
+        var points = input.Select(l => l.Split(',').Select(long.Parse))
+     .Select(v => new Point(v))
+     .ToList();
+
+        var checkedEmptyPoints = new List<Point>();
+
+        var minX = points.Select(p => p.X).Min();
+        var maxX = points.Select(p => p.X).Max();
+        var minY = points.Select(p => p.Y).Min();
+        var maxY = points.Select(p => p.Y).Max();
+        var minZ = points.Select(p => p.Z).Min();
+        var maxZ = points.Select(p => p.Z).Max();
+
+        var faceCount = 0;
+
+        var isInsight = false;
+
+        var pointsToMark = new List<Point>();
+
+        pointsChecked = new List<Point>();
+
+        for (var z = minZ; z <= maxZ; z++)
+        {
+            for (var y = minY; y <= maxY; y++)
+            {
+                for (var x = minX; x <= maxX; x++)
+                {
+                    if (points.Any(p => p.X == x && p.Y == y && p.Z == z))
+                        continue;
+
+                    if (checkedEmptyPoints.Any(p => p.X == x && p.Y == y && p.Z == z) == false)
+                    {
+                        if (x==2 && y==2 && z==5)
+                        {
+
+                        }
+
+                        if (CheckPoints(points, x, y, z, maxX, maxY, maxZ, minX, minY, minZ) == true)
+                        {
+                            MarkPoints(points, x, y, z, maxX, maxY, maxZ, minX, minY, minZ);
+                        }
+                        else
+                        {
+                            checkedEmptyPoints.Add(new Point(x, y, z));
+                        }
+                    }
+                }
+            }
+        }
+
+        for (var z = minZ; z <= maxZ; z++)
+        {
+            for (var y = minY; y <= maxY; y++)
+            {
+                for (var x = minX; x <= maxX; x++)
+                {
+                    if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true && points.Any(p => p.X == x + 1 && p.Y == y && p.Z == z) == false)
+                        faceCount++;
+
+                    if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true && points.Any(p => p.X == x - 1 && p.Y == y && p.Z == z) == false)
+                        faceCount++;
+
+                    if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true && points.Any(p => p.X == x && p.Y == y + 1 && p.Z == z) == false)
+                        faceCount++;
+
+                    if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true && points.Any(p => p.X == x && p.Y == y - 1 && p.Z == z) == false)
+                        faceCount++;
+
+                    if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true && points.Any(p => p.X == x && p.Y == y && p.Z == z + 1) == false)
+                        faceCount++;
+
+                    if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true && points.Any(p => p.X == x && p.Y == y && p.Z == z - 1) == false)
+                        faceCount++;
+                }
+            }
+        }
+
+        // 4136 too high
+        return faceCount.ToString();
+    }
+
+    static List<Point> pointsChecked = new List<Point>();
+
+    static bool CheckPoints(List<Point> points, long x, long y, long z, long maxX, long maxY, long maxZ, long minX, long minY, long minZ)
+    {   
+        if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true)
+            return true;
+
+        if (pointsChecked.Any(p => p.X == x && p.Y == y && p.Z == z) == true)
+            return false;
+
+        pointsChecked.Add(new Point(x, y, z));
+
+
+
+        if (x < minX || x > maxX || y < minY || y > maxY || z < minZ || z > maxZ)
+            return false;
+
+        var result = true;
+
+        result &= CheckPoints(points, x + 1, y, z, maxX, maxY, maxZ, minX, minY, minZ);
+        result &= CheckPoints(points, x - 1, y, z, maxX, maxY, maxZ, minX, minY, minZ);
+        result &= CheckPoints(points, x, y + 1, z, maxX, maxY, maxZ, minX, minY, minZ);
+        result &= CheckPoints(points, x, y - 1, z, maxX, maxY, maxZ, minX, minY, minZ);
+        result &= CheckPoints(points, x, y, z + 1, maxX, maxY, maxZ, minX, minY, minZ);
+        result &= CheckPoints(points, x, y, z - 1, maxX, maxY, maxZ, minX, minY, minZ);
+
+        return result;
+    }
+
+    static void MarkPoints(List<Point> points, long x, long y, long z, long maxX, long maxY, long maxZ, long minX, long minY, long minZ)
+    {
+        if (points.Any(p => p.X == x && p.Y == y && p.Z == z) == true)
+            return;
+
+        if (x < minX || x > maxX || y < minY || y > maxY || z < minZ || z > maxZ)
+            return;
+
+        points.Add(new Point(x, y, z));
+
+        MarkPoints(points, x + 1, y, z, maxX, maxY, maxZ, minX, minY, minZ);
+        MarkPoints(points, x - 1, y, z, maxX, maxY, maxZ, minX, minY, minZ);
+        MarkPoints(points, x, y + 1, z, maxX, maxY, maxZ, minX, minY, minZ);
+        MarkPoints(points, x, y - 1, z, maxX, maxY, maxZ, minX, minY, minZ);
+        MarkPoints(points, x, y, z + 1, maxX, maxY, maxZ, minX, minY, minZ);
+        MarkPoints(points, x, y, z - 1, maxX, maxY, maxZ, minX, minY, minZ);
     }
 
     static string[] GetInput(string inputPath) =>
