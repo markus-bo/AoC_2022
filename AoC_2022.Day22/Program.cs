d43033c11c04c490c8c0453f9eb9e5b5b12a02fc
Markus Bodlos <m.bodlos@incubedit.com>
2022-12-22 14:12:16 +0100

    Template for Day 22 created

---
+++ AoC_2022.Day22/Program.cs
@@ -0,0 +1,30 @@
+﻿class Solution
+{
+    static void Main(string[] args)
+    {
+        var input0 = GetInput($"{Environment.CurrentDirectory}\\Input0.txt");
+        var input1 = GetInput($"{Environment.CurrentDirectory}\\Input1.txt");
+
+        Console.WriteLine($"Part 1, input 0: '{solutionPart1(input0)}'");
+        Console.WriteLine($"Part 1, input 1: '{solutionPart1(input1)}'");
+        Console.WriteLine($"Part 2, input 0: '{solutionPart2(input0)}'");
+        Console.WriteLine($"Part 2, input 1: '{solutionPart2(input1)}'");
+    }
+
+    static string solutionPart1(string[] input)
+    {
+        return "";
+    }
+
+    static string solutionPart2(string[] input)
+    {
+        return "";
+    }
+
+    static string[] GetInput(string inputPath) =>
+        new StreamReader(inputPath)
+            .ReadToEnd()
+            .Split('\n')
+            .Select(x => x.Trim('\r'))
+            .ToArray();
+}
